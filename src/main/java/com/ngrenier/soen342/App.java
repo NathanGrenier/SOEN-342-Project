caf201fc46593a5f929dae962ed6dd72215b8da8
NathanGrenier <nathangrenier01@gmail.com>
2024-11-13 17:47:17 -0500

    1: Revised the terminal class
    Co-authored-by: Nathanial Hwong <nathanial-hwong@users.noreply.github.com>

--- src/main/java/com/ngrenier/soen342/App.java
+++ src/main/java/com/ngrenier/soen342/App.java
@@ -1,8 +1,11 @@
 package com.ngrenier.soen342;
 
 import com.ngrenier.soen342.services.AuthenticationService;
+import com.ngrenier.soen342.users.Admin;
 import com.ngrenier.soen342.users.AdminRecords;
+import com.ngrenier.soen342.users.Client;
 import com.ngrenier.soen342.users.ClientRecords;
+import com.ngrenier.soen342.users.Instructor;
 import com.ngrenier.soen342.users.InstructorRecords;
 import com.ngrenier.soen342.users.User;
 
@@ -18,53 +21,71 @@ public class App {
     private BookingRecords bookingRecords = BookingRecords.getInstance();
 
     private static App instance = new App();
-    public App(){
+
+    public App() {
     }
+
     public static App getInstance() {
         return instance;
     }
-    public User getCurrentUser() {
-        return currentUser;
-    }
 
-    public void setCurrentUser(User currentUser) {
-        this.currentUser = currentUser;
-    }
-    
     public void viewClientOfferings() {
-        
+
         System.out.println("Viewing offerings as Client...");
     }
 
-    public void bookClientOffering(String offering) {
-        System.out.println("Booking offering: " + offering);
+    public void bookClientOffering() {
+        // System.out.println("Booking offering: " + offering);
     }
 
     public void viewClientBookings() {
         System.out.println("Viewing Client bookings...");
     }
 
-    public void cancelClientBooking(String booking) {
-        System.out.println("Cancelling Client booking: " + booking);
+    public void cancelClientBooking() {
+        // System.out.println("Cancelling Client booking: " + booking);
     }
 
     public void viewInstructorOfferings() {
         System.out.println("Viewing offerings as Instructor...");
     }
 
-    public void acceptInstructorLesson(String lesson) {
-        System.out.println("Accepting lesson: " + lesson);
+    public void acceptInstructorLesson() {
+        // System.out.println("Accepting lesson: " + lesson);
     }
-    //admin methods
+
+    // admin methods
     public void viewAllBookings() {
         System.out.println("Viewing all bookings as Admin...");
     }
 
-    public void editOfferings(String[] input) {
+    public void editOfferings() {
         System.out.println("Editing offerings as Admin...");
     }
 
-    public void deleteUser(String user) {
-        System.out.println("Deleting user: " + user);
+    public void deleteUser() {
+        // System.out.println("Deleting user: " + user);
+    }
+
+    public String getCurrentUserType() {
+        if (currentUser instanceof Admin) {
+            return "Admin";
+        } else if (currentUser instanceof Instructor) {
+            return "Instructor";
+        } else if (currentUser instanceof Client) {
+            return "Client";
+        } else if (currentUser == null) {
+            return "Public";
+        } else {
+            return "Unknown";
+        }
+    }
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public void setCurrentUser(User currentUser) {
+        this.currentUser = currentUser;
     }
 }
